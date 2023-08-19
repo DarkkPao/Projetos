70788295b2fbbfb87f068a43b01684eb6d2c87c9
Vitor <agente51411@gmail.com>
2023-08-19 18:56:09 -0300

    Calculadora IMC

---
+++ CalculadoraIMC/index.js
@@ -0,0 +1,7 @@
+function calcularIMC() {
+  const altura = parseFloat(document.getElementById("altura").value);
+  const peso = parseFloat(document.getElementById("peso").value);
+  const IMC = (peso / Math.pow(altura, 2)).toFixed(2);
+
+  document.getElementById("resultado").textContent = IMC;
+}
